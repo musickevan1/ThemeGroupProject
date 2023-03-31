a6c77e56be2d00cc329d08559b305dea83e28c93
Zac <zfirebender@gmail.com>
2023-03-31 13:59:29 -0500

    Did some theme changes for the second activity.

--- app/src/main/java/com/example/themegroupproject/MainActivity2.java
+++ app/src/main/java/com/example/themegroupproject/MainActivity2.java
@@ -3,9 +3,12 @@ package com.example.themegroupproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Switch;
+import android.widget.TextView;
 
-public class MainActivity2 extends AppCompatActivity {
-
+public class MainActivity2 extends MainActivity {
+    Switch themeTog;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
